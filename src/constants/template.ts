47388682759666c18c466447a0b3450a64270e0d
lghuy05 <luonggiahuy1909@gmail.com>
2025-12-28 00:37:28 -0500

    templates

---
+++ src/constants/template.ts
@@ -0,0 +1,38 @@
+
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank Document",
+    imageUrl: "/default_doc.svg"
+  },
+  {
+    id: "software-proposal",
+    label: "Software Development proposal",
+    imageUrl: "/software_proposal.svg"
+  },
+  {
+    id: "project-proposal",
+    label: "Project proposal",
+    imageUrl: "/proposal.svg"
+  },
+  {
+    id: "business-letter",
+    label: "Business letter",
+    imageUrl: "/business_letter.svg"
+  },
+  {
+    id: "resume",
+    label: "Resume",
+    imageUrl: "/resume.svg"
+  },
+  {
+    id: "cover-letter",
+    label: "Cover letter",
+    imageUrl: "/cover_letter.svg"
+  },
+  {
+    id: "letter",
+    label: "Letter",
+    imageUrl: "/letter.svg"
+  },
+];
