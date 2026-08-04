af8190a6d4b9d8f77bcaeb89206b816ab430d6ee
lghuy05 <luonggiahuy1909@gmail.com>
2026-08-04 15:19:47 +0700

    refactor: modularize Tiptap Vim extension

---
+++ src/extensions/vim/types.ts
@@ -0,0 +1,56 @@
+import type { Slice } from "@tiptap/pm/model";
+
+export type VimMode = "normal" | "insert" | "visual";
+
+export interface VimModeOptions {
+  onQuit?: () => void;
+}
+
+export interface PendingOp {
+  key: "d" | "y";
+  expires: number;
+}
+
+export interface PendingOpMotion {
+  op: "c" | "d" | "y";
+  expires: number;
+}
+
+export interface PendingMotion {
+  key: "g";
+  expires: number;
+}
+
+export interface PendingFind {
+  dir: 1 | -1;
+  expires: number;
+  type: "f" | "t";
+}
+
+export interface LastFind {
+  dir: 1 | -1;
+  char: string;
+  type: "f" | "t";
+}
+
+export interface PendingWordOp {
+  op: "c" | "d" | "v";
+  step: "i" | "w";
+  expires: number;
+}
+
+export interface VimModeStorage {
+  enabled: boolean;
+  mode: VimMode;
+  visualAnchor: number | null;
+  yankSlice: Slice | null;
+  pendingOp: PendingOp | null;
+  pendingOpMotion: PendingOpMotion | null;
+  pendingMotion: PendingMotion | null;
+  pendingFind: PendingFind | null;
+  lastFind: LastFind | null;
+  pendingWordOp: PendingWordOp | null;
+  commandActive: boolean;
+  commandBuffer: string;
+  searchQuery: string;
+}
